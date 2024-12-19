99617d31a7be86dc855261d13c016c37826591d9
eliseo.nava <eliseo.nava@cbtis128.edu.mx>
2024-12-19 12:22:30 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'fichaproducto.dart';
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:884209073.
+void main() => runApp(const AppProducto());
+
+class AppProducto extends StatelessWidget {
+  const AppProducto({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aplicacion producto',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const FichaProducto(
+          nombre: 'Producto ejemplo',
+          descripcion: 'Breve descripcion del producto',
+          precio: 21,
+          imagen: 'producto.png'),
+    );
+  }
+}
